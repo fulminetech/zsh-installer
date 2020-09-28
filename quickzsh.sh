0d9d0393eb03e9159d5d68796facebb9ce4c5efc
fulminetech <71808779+fulminetech@users.noreply.github.com>
2020-09-28 15:49:34 +0530

    Create quickzsh.sh

---
+++ quickzsh.sh
@@ -0,0 +1,21 @@
+# Install ZSH
+sudo apt install zsh -y
+
+# Install OhMyZsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Change zsh to default
+chsh -s $(which zsh)
+
+# Install auto-suggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Further instructions 
+echo [ UPDATE at ~/.zshrc  ]
+echo "plugins=(git
+zsh-autosuggestions
+zsh-syntax-highlighting
+)"
